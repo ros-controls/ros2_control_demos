36a01d0e9f53f6acab3177c680200f456f67fa19
Denis Štogl <denis@stogl.de>
2020-06-01 20:21:30 +0200

    Try to start Demo Robot

--- ros2_control_demo_robot/launch/demo_robot.launch.py
+++ ros2_control_demo_robot/launch/demo_robot.launch.py
@@ -1,13 +1,13 @@
 # Copyright 2020 ROS2-Control Development Team
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
+# Licensed under the Apache License, Version 2.0 (the 'License');
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
 #    http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
+# distributed under the License is distributed on an 'AS IS' BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
@@ -25,17 +25,25 @@ def generate_launch_description():
     ld = LaunchDescription()
 
     demo_robot_config = os.path.join(
-        get_package_share_directory("ros2_control_demo_robot"),
-        "config",
-        "demo_robot.yaml"
+        get_package_share_directory('ros2_control_demo_robot'),
+        'config',
+        'demo_robot.yaml'
         )
 
     return LaunchDescription([
       Node(
-        package="ros2_control_demo_robot",
-        node_name="ros2_control_demo_robot_manager",
-        node_executable="ros2_control_demo_robot_manager",
-        parameters=[demo_robot_config]
+        package='ros2_control_demo_robot',
+        node_name='ros2_control_demo_robot_manager',
+        node_executable='ros2_control_demo_robot_manager',
+        parameters=[demo_robot_config],
+        output={
+          'stdout': 'screen',
+          'stderr': 'screen',
+          },
+        #log_level={
+          #'logger_name': '',
+          #'level': 'DEBUG'
+          #}
         )
 
     ])
--- ros2_control_demo_robot/src/ros2_control_demo_robot_manager.cpp
+++ ros2_control_demo_robot/src/ros2_control_demo_robot_manager.cpp
@@ -29,13 +29,31 @@ class ROS2ControlCoreTestClass: public rclcpp::Node
 public:
   ROS2ControlCoreTestClass(rclcpp::NodeOptions options) : Node("ros2_control_core_test_node", options)
   {
-    robot_.reset(new ros2_control_core_components::Robot(std::string("TestRobot"), this->get_node_logging_interface(), this->get_node_parameters_interface(), this->get_node_services_interface(), std::string("DemoRobot")));
+    robot_.reset(new ros2_control_core_components::Robot());
+    robot_->configure(std::string("DemoRobot"), this->get_node_logging_interface(), this->get_node_parameters_interface(), this->get_node_services_interface());
+
+    parameters_client_ = std::make_shared<rclcpp::SyncParametersClient>(this);
+    while (!parameters_client_->wait_for_service(1s)) {
+      if (!rclcpp::ok()) {
+        RCLCPP_ERROR(this->get_logger(), "Interrupted while waiting for the service. Exiting.");
+        rclcpp::shutdown();
+      }
+      RCLCPP_INFO(this->get_logger(), "service not available, waiting again...");
+    }
+
+    robot_->init();
 
     timer_ = this->create_wall_timer(1000ms, std::bind(&ROS2ControlCoreTestClass::loop, this));
   }
 
   void loop()
   {
+//     robot_->read();
+//
+//     update_values();
+
+
+
     robot_->recover();
 
     RCLCPP_INFO(this->get_logger(), "Calling loop...");
@@ -44,15 +62,21 @@ public:
 private:
   rclcpp::TimerBase::SharedPtr timer_;
   std::shared_ptr<ros2_control_core_components::Robot> robot_;
+  std::shared_ptr<rclcpp::SyncParametersClient> parameters_client_;
+  control_msgs::msg::InterfaceValue values_;
 
+//   void update_values()
+//   {
+//
+//   }
 };
 
 int main(int argc, char** argv)
 {
   rclcpp::init(argc, argv);
   rclcpp::NodeOptions options;
-  options.allow_undeclared_parameters(true);
-  options.automatically_declare_parameters_from_overrides(true);
+//   options.allow_undeclared_parameters(true);
+//   options.automatically_declare_parameters_from_overrides(true);
   rclcpp::spin(std::make_shared<ROS2ControlCoreTestClass>(options));
   rclcpp::shutdown();
   return 0;
