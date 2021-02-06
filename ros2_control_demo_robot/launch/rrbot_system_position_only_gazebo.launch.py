67be5c547c3239e7fb2afe31be18ed9b5b9131ce
Olivier Stasse <ostasse@laas.fr>
2021-02-06 11:23:49 +0100

    [ros2_control_demo_robot] Add missing launch file and controller position file.

---
+++ ros2_control_demo_robot/launch/rrbot_system_position_only_gazebo.launch.py
@@ -0,0 +1,62 @@
+# Copyright 2020 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+
+import xacro
+
+
+def generate_launch_description():
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
+             )
+
+    ros2_control_demos_path = os.path.join(
+        get_package_share_directory('ros2_control_demos'))
+
+    robot_description_path = os.path.join(
+        get_package_share_directory('ros2_control_demo_robot'),
+        'description',
+        'rrbot_system_position_only.urdf.xacro')
+    robot_description_config = xacro.process_file(robot_description_path,
+    mappings={'simu' : 'true'})
+    robot_description = {'robot_description': robot_description_config.toxml()}
+
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[robot_description]
+    )
+
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'cartpole'],
+                        output='screen')
+
+    return LaunchDescription([
+        gazebo,
+        node_robot_state_publisher,
+        spawn_entity,
+    ])
