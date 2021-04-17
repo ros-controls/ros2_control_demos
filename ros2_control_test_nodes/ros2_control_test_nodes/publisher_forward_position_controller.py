8f1af3726ab1b8da2b73824a29e643f318e6e82a
Denis Štogl <denis@stogl.de>
2021-04-17 14:07:27 +0200

    Update for linters

--- ros2_control_test_nodes/ros2_control_test_nodes/publisher_forward_position_controller.py
+++ ros2_control_test_nodes/ros2_control_test_nodes/publisher_forward_position_controller.py
@@ -19,18 +19,17 @@ from std_msgs.msg import Float64MultiArray
 
 
 class PublisherForwardPosition(Node):
-
     def __init__(self):
-        super().__init__('publisher_forward_position_controller')
+        super().__init__("publisher_forward_position_controller")
         # Declare all parameters
-        self.declare_parameter('controller_name', "forward_position_controller")
-        self.declare_parameter('wait_sec_between_publish', 5)
-        self.declare_parameter('goal_names', ['pos1', 'pos2'])
+        self.declare_parameter("controller_name", "forward_position_controller")
+        self.declare_parameter("wait_sec_between_publish", 5)
+        self.declare_parameter("goal_names", ["pos1", "pos2"])
 
         # Read parameters
-        controller_name = self.get_parameter('controller_name').value
-        wait_sec_between_publish = self.get_parameter('wait_sec_between_publish').value
-        goal_names = self.get_parameter('goal_names').value
+        controller_name = self.get_parameter("controller_name").value
+        wait_sec_between_publish = self.get_parameter("wait_sec_between_publish").value
+        goal_names = self.get_parameter("goal_names").value
 
         # Read all positions from parameters
         self.goals = []
@@ -38,7 +37,7 @@ class PublisherForwardPosition(Node):
             self.declare_parameter(name)
             goal = self.get_parameter(name).value
             if goal is None or len(goal) == 0:
-                raise Exception('Values for goal "{}" not set!'.format(name))
+                raise Exception(f"Values for goal '{name}' not set!")
 
             float_goal = []
             for value in goal:
@@ -48,18 +47,17 @@ class PublisherForwardPosition(Node):
         publish_topic = "/" + controller_name + "/" + "commands"
 
         self.get_logger().info(
-            'Publishing {} goals on topic "{}" every {} s'.format(
-                len(goal_names), publish_topic, wait_sec_between_publish))
+            f"Publishing {len(goal_names)} goals on topic '{publish_topic}' every {wait_sec_between_publish} s"
+        )
 
-        self.publisher_ = self.create_publisher(
-            Float64MultiArray, publish_topic, 1)
+        self.publisher_ = self.create_publisher(Float64MultiArray, publish_topic, 1)
         self.timer = self.create_timer(wait_sec_between_publish, self.timer_callback)
         self.i = 0
 
     def timer_callback(self):
         msg = Float64MultiArray()
         msg.data = self.goals[self.i]
-        self.get_logger().info('Publishing: "{}"'.format(msg.data))
+        self.get_logger().info(f"Publishing: '{msg.data}'")
         self.publisher_.publish(msg)
         self.i += 1
         self.i %= len(self.goals)
@@ -75,5 +73,5 @@ def main(args=None):
     rclpy.shutdown()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
