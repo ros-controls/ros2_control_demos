c1ae3e6687c10ba1b9843afdd6164e362a15e141
Mathias Hauan Arbo <mathias.arbo@itk.ntnu.no>
2021-04-23 11:52:31 +0200

    prepare - perform switch

---
+++ ros2_control_demo_robot/launch/rrbot_system_multi_interface.launch.py
@@ -0,0 +1,37 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+import xacro
+
+
+def generate_launch_description():
+
+  # Get URDF via xacro
+  robot_description_path = os.path.join(
+    get_package_share_directory('ros2_control_demo_robot'),
+    'description',
+    'rrbot_system_multi_interface.urdf.xacro')
+  robot_description_config = xacro.process_file(robot_description_path)
+  robot_description = {'robot_description': robot_description_config.toxml()}
+
+  rrbot_forward_controller = os.path.join(
+    get_package_share_directory('ros2_control_demo_robot'),
+    'config',
+    'rrbot_multi_interface_forward_controllers.yaml'
+  )
+
+  return LaunchDescription([
+    Node(
+      package='controller_manager',
+      executable='ros2_control_node',
+      parameters=[robot_description, rrbot_forward_controller],
+      output={
+        'stdout': 'screen',
+        'stderr': 'screen'
+      }
+    )
+  ])
