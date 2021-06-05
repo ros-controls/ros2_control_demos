ef20574f579db4a7a772c49f3e23f4a420da28c1
Olivier Stasse <olivier.stasse@laas.fr>
2021-06-05 08:16:16 +0000

    Fix launch file style.

--- ros2_control_demo_description/launch/view_robot.launch.py
+++ ros2_control_demo_description/launch/view_robot.launch.py
@@ -58,7 +58,9 @@ def generate_launch_description():
         [
             PathJoinSubstitution([FindExecutable(name="xacro")]),
             " ",
-            PathJoinSubstitution([FindPackageShare(description_package), "urdf", description_file]),
+            PathJoinSubstitution(
+                [FindPackageShare(description_package), "urdf", description_file]
+            ),
             " ",
             "prefix:=",
             prefix,
