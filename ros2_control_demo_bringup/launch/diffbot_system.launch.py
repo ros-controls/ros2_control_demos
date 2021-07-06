f91ac84b7b457c7cdc5dba3a78f3b43752356b65
Denis Stogl <denis@stogl.de>
2021-07-06 14:42:38 +0200

    Simplify diffbot example.

--- ros2_control_demo_bringup/launch/diffbot_system.launch.py
+++ ros2_control_demo_bringup/launch/diffbot_system.launch.py
@@ -26,52 +26,6 @@ def generate_launch_description():
     declared_arguments = []
     declared_arguments.append(
         DeclareLaunchArgument(
-            "runtime_config_package",
-            default_value="ros2_control_demo_bringup",
-            description='Package with the controller\'s configuration in "config" folder. \
-        Usually the argument is not set, it enables use of a custom setup.',
-        )
-    )
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            "controllers_file",
-            default_value="diffbot_diff_drive_controller.yaml",
-            description="YAML file with the controllers configuration.",
-        )
-    )
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            "description_package",
-            default_value="diffbot_description",
-            description="Description package with robot URDF/xacro files. Usually the argument \
-        is not set, it enables use of a custom description.",
-        )
-    )
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            "description_file",
-            default_value="diffbot_system.urdf.xacro",
-            description="URDF/XACRO description file with the robot.",
-        )
-    )
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            "prefix",
-            default_value='""',
-            description="Prefix of the joint names, useful for \
-        multi-robot setup. If changed than also joint names in the controllers' configuration \
-        have to be updated.",
-        )
-    )
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            "robot_controller",
-            default_value="diffbot_base_controller",
-            description="Robot controller to start.",
-        )
-    )
-    declared_arguments.append(
-        DeclareLaunchArgument(
             "start_rviz",
             default_value="true",
             description="Start RViz2 automatically with this launch file.",
@@ -79,12 +33,6 @@ def generate_launch_description():
     )
 
     # Initialize Arguments
-    runtime_config_package = LaunchConfiguration("runtime_config_package")
-    controllers_file = LaunchConfiguration("controllers_file")
-    description_package = LaunchConfiguration("description_package")
-    description_file = LaunchConfiguration("description_file")
-    prefix = LaunchConfiguration("prefix")
-    robot_controller = LaunchConfiguration("robot_controller")
     start_rviz = LaunchConfiguration("start_rviz")
 
     # Get URDF via xacro
@@ -93,25 +41,21 @@ def generate_launch_description():
             PathJoinSubstitution([FindExecutable(name="xacro")]),
             " ",
             PathJoinSubstitution(
-                [FindPackageShare(description_package), "urdf", description_file]
+                [FindPackageShare("diffbot_description"), "urdf", "diffbot_system.urdf.xacro"]
             ),
-            " ",
-            "prefix:=",
-            prefix,
-            " ",
         ]
     )
     robot_description = {"robot_description": robot_description_content}
 
     robot_controllers = PathJoinSubstitution(
         [
-            FindPackageShare(runtime_config_package),
+            FindPackageShare("ros2_control_demo_bringup"),
             "config",
-            controllers_file,
+            "diffbot_diff_drive_controller.yaml",
         ]
     )
     rviz_config_file = PathJoinSubstitution(
-        [FindPackageShare(description_package), "config", "diffbot.rviz"]
+        [FindPackageShare("diffbot_description"), "config", "diffbot.rviz"]
     )
 
     control_node = Node(
@@ -147,7 +91,7 @@ def generate_launch_description():
     robot_controller_spawner = Node(
         package="controller_manager",
         executable="spawner.py",
-        arguments=[robot_controller, "-c", "/controller_manager"],
+        arguments=["diffbot_base_controller", "-c", "/controller_manager"],
     )
 
     nodes = [
