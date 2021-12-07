94cb02b7bb320139c1821dd2f0d9d7023ada5390
Denis Štogl <destogl@users.noreply.github.com>
2021-12-07 14:24:23 +0100

    Apply suggestions from code review

--- ros2_control_demo_bringup/launch/rrbot_modular_actuators.launch.py
+++ ros2_control_demo_bringup/launch/rrbot_modular_actuators.launch.py
@@ -1,4 +1,4 @@
-# Copyright 2021 Department of Engineering Cybernetics, NTNU.
+# Copyright (c) 2021, Stogl Robotics Consulting UG (haftungsbeschränkt)
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
