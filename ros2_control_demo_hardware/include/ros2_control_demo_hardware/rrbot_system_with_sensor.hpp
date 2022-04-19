6d841dffcfadf07cff779862a64fc292e9ae62a1
br33x <98892946+br33x@users.noreply.github.com>
2022-04-19 22:02:43 +0200

    CallbackReturn fix_v2

--- ros2_control_demo_hardware/include/ros2_control_demo_hardware/rrbot_system_with_sensor.hpp
+++ ros2_control_demo_hardware/include/ros2_control_demo_hardware/rrbot_system_with_sensor.hpp
@@ -40,10 +40,10 @@ public:
   RCLCPP_SHARED_PTR_DEFINITIONS(RRBotSystemWithSensorHardware);
 
   ROS2_CONTROL_DEMO_HARDWARE_PUBLIC
-  CallbackReturn on_init(const hardware_interface::HardwareInfo & info) override;
+  hardware_interface::CallbackReturn on_init(const hardware_interface::HardwareInfo & info) override;
 
   ROS2_CONTROL_DEMO_HARDWARE_PUBLIC
-  CallbackReturn on_configure(const rclcpp_lifecycle::State & previous_state) override;
+  hardware_interface::CallbackReturn on_configure(const rclcpp_lifecycle::State & previous_state) override;
 
   ROS2_CONTROL_DEMO_HARDWARE_PUBLIC
   std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
@@ -52,10 +52,10 @@ public:
   std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
 
   ROS2_CONTROL_DEMO_HARDWARE_PUBLIC
-  CallbackReturn on_activate(const rclcpp_lifecycle::State & previous_state) override;
+  hardware_interface::CallbackReturn on_activate(const rclcpp_lifecycle::State & previous_state) override;
 
   ROS2_CONTROL_DEMO_HARDWARE_PUBLIC
-  CallbackReturn on_deactivate(const rclcpp_lifecycle::State & previous_state) override;
+  hardware_interface::CallbackReturn on_deactivate(const rclcpp_lifecycle::State & previous_state) override;
 
   ROS2_CONTROL_DEMO_HARDWARE_PUBLIC
   hardware_interface::return_type read() override;
