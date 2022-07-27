943139ab9c3df12a6c475ec9f064703a568da813
Paul Gesel <paul.gesel@picknik.ai>
2022-07-27 14:42:16 -0600

    rename robot to r6bot

---
+++ r6bot/reference_generator/launch/send_trajectory.launch.py
@@ -0,0 +1,33 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    # Get URDF via xacro
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            " ",
+            PathJoinSubstitution(
+                [
+                    FindPackageShare("r6bot_description"),
+                    "urdf",
+                    "r6bot.urdf.xacro",
+                ]
+            ),
+        ]
+    )
+    robot_description = {"robot_description": robot_description_content}
+
+    send_trajectory_node = Node(
+        package="reference_generator",
+        executable="send_trajectory",
+        name="send_trajectory_node",
+        parameters=[robot_description],
+    )
+
+    nodes_to_start = [send_trajectory_node]
+    return LaunchDescription(nodes_to_start)
