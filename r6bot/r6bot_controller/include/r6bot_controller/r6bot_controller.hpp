c961d03a12cc275426e28dafb3119539ebd4a9fc
Paul Gesel <paul.gesel@picknik.ai>
2022-07-28 08:46:01 -0600

    clang-format

--- r6bot/r6bot_controller/include/r6bot_controller/r6bot_controller.hpp
+++ r6bot/r6bot_controller/include/r6bot_controller/r6bot_controller.hpp
@@ -21,80 +21,84 @@
 #include "trajectory_msgs/msg/joint_trajectory.hpp"
 #include "trajectory_msgs/msg/joint_trajectory_point.hpp"
 
-namespace r6bot_controller {
+namespace r6bot_controller
+{
+class RobotController : public controller_interface::ControllerInterface
+{
+public:
+  CONTROLLER_INTERFACE_PUBLIC
+  RobotController();
 
-  class RobotController : public controller_interface::ControllerInterface {
-  public:
-    CONTROLLER_INTERFACE_PUBLIC
-    RobotController();
+  CONTROLLER_INTERFACE_PUBLIC
+  controller_interface::InterfaceConfiguration command_interface_configuration() const override;
 
-    CONTROLLER_INTERFACE_PUBLIC
-    controller_interface::InterfaceConfiguration command_interface_configuration() const override;
+  CONTROLLER_INTERFACE_PUBLIC
+  controller_interface::InterfaceConfiguration state_interface_configuration() const override;
 
-    CONTROLLER_INTERFACE_PUBLIC
-    controller_interface::InterfaceConfiguration state_interface_configuration() const override;
+  CONTROLLER_INTERFACE_PUBLIC
+  controller_interface::return_type update(
+    const rclcpp::Time & time, const rclcpp::Duration & period) override;
 
-    CONTROLLER_INTERFACE_PUBLIC
-    controller_interface::return_type update(
-        const rclcpp::Time &time, const rclcpp::Duration &period) override;
+  CONTROLLER_INTERFACE_PUBLIC
+  controller_interface::CallbackReturn on_init() override;
 
-    CONTROLLER_INTERFACE_PUBLIC
-    controller_interface::CallbackReturn on_init() override;
+  CONTROLLER_INTERFACE_PUBLIC
+  controller_interface::CallbackReturn on_configure(
+    const rclcpp_lifecycle::State & previous_state) override;
 
-    CONTROLLER_INTERFACE_PUBLIC
-    controller_interface::CallbackReturn on_configure(
-        const rclcpp_lifecycle::State &previous_state) override;
+  CONTROLLER_INTERFACE_PUBLIC
+  controller_interface::CallbackReturn on_activate(
+    const rclcpp_lifecycle::State & previous_state) override;
 
-    CONTROLLER_INTERFACE_PUBLIC
-    controller_interface::CallbackReturn on_activate(
-        const rclcpp_lifecycle::State &previous_state) override;
+  CONTROLLER_INTERFACE_PUBLIC
+  controller_interface::CallbackReturn on_deactivate(
+    const rclcpp_lifecycle::State & previous_state) override;
 
-    CONTROLLER_INTERFACE_PUBLIC
-    controller_interface::CallbackReturn on_deactivate(
-        const rclcpp_lifecycle::State &previous_state) override;
+  CONTROLLER_INTERFACE_PUBLIC
+  controller_interface::CallbackReturn on_cleanup(
+    const rclcpp_lifecycle::State & previous_state) override;
 
-    CONTROLLER_INTERFACE_PUBLIC
-    controller_interface::CallbackReturn on_cleanup(
-        const rclcpp_lifecycle::State &previous_state) override;
+  CONTROLLER_INTERFACE_PUBLIC
+  controller_interface::CallbackReturn on_error(
+    const rclcpp_lifecycle::State & previous_state) override;
 
-    CONTROLLER_INTERFACE_PUBLIC
-    controller_interface::CallbackReturn on_error(
-        const rclcpp_lifecycle::State &previous_state) override;
+  CONTROLLER_INTERFACE_PUBLIC
+  controller_interface::CallbackReturn on_shutdown(
+    const rclcpp_lifecycle::State & previous_state) override;
 
-    CONTROLLER_INTERFACE_PUBLIC
-    controller_interface::CallbackReturn on_shutdown(
-        const rclcpp_lifecycle::State &previous_state) override;
+protected:
+  std::vector<std::string> joint_names_;
+  std::vector<std::string> command_interface_types_;
+  std::vector<std::string> state_interface_types_;
 
-  protected:
-    std::vector<std::string> joint_names_;
-    std::vector<std::string> command_interface_types_;
-    std::vector<std::string> state_interface_types_;
+  rclcpp::Subscription<trajectory_msgs::msg::JointTrajectory>::SharedPtr joint_command_subscriber_;
+  realtime_tools::RealtimeBuffer<std::shared_ptr<trajectory_msgs::msg::JointTrajectory>>
+    traj_msg_external_point_ptr_;
+  bool new_msg_ = false;
+  rclcpp::Time start_time_;
+  std::shared_ptr<trajectory_msgs::msg::JointTrajectory> trajectory_msg_;
+  trajectory_msgs::msg::JointTrajectoryPoint point_interp_;
 
-    rclcpp::Subscription<trajectory_msgs::msg::JointTrajectory>::SharedPtr joint_command_subscriber_;
-    realtime_tools::RealtimeBuffer<std::shared_ptr<trajectory_msgs::msg::JointTrajectory>> traj_msg_external_point_ptr_;
-    bool new_msg_ = false;
-    rclcpp::Time start_time_;
-    std::shared_ptr<trajectory_msgs::msg::JointTrajectory> trajectory_msg_;
-    trajectory_msgs::msg::JointTrajectoryPoint point_interp_;
+  std::vector<std::reference_wrapper<hardware_interface::LoanedCommandInterface>>
+    joint_position_command_interface_;
+  std::vector<std::reference_wrapper<hardware_interface::LoanedCommandInterface>>
+    joint_velocity_command_interface_;
+  std::vector<std::reference_wrapper<hardware_interface::LoanedStateInterface>>
+    joint_position_state_interface_;
+  std::vector<std::reference_wrapper<hardware_interface::LoanedStateInterface>>
+    joint_velocity_state_interface_;
 
-    std::vector<std::reference_wrapper<hardware_interface::LoanedCommandInterface>> joint_position_command_interface_;
-    std::vector<std::reference_wrapper<hardware_interface::LoanedCommandInterface>> joint_velocity_command_interface_;
-    std::vector<std::reference_wrapper<hardware_interface::LoanedStateInterface>> joint_position_state_interface_;
-    std::vector<std::reference_wrapper<hardware_interface::LoanedStateInterface>> joint_velocity_state_interface_;
+  std::unordered_map<
+    std::string, std::vector<std::reference_wrapper<hardware_interface::LoanedCommandInterface>> *>
+    command_interface_map_ = {
+      {"position", &joint_position_command_interface_},
+      {"velocity", &joint_velocity_command_interface_}};
 
-    std::unordered_map<std::string,
-        std::vector<std::reference_wrapper<hardware_interface::LoanedCommandInterface >> *>
-        command_interface_map_ = {
-        {"position", &joint_position_command_interface_},
-        {"velocity", &joint_velocity_command_interface_}
-    };
+  std::unordered_map<
+    std::string, std::vector<std::reference_wrapper<hardware_interface::LoanedStateInterface>> *>
+    state_interface_map_ = {
+      {"position", &joint_position_state_interface_},
+      {"velocity", &joint_velocity_state_interface_}};
+};
 
-    std::unordered_map<std::string,
-        std::vector<std::reference_wrapper<hardware_interface::LoanedStateInterface >> *> state_interface_map_ = {
-        {"position", &joint_position_state_interface_},
-        {"velocity", &joint_velocity_state_interface_}
-    };
-
-  };
-
-}  // namespace joint_trajectory_controller
+}  // namespace r6bot_controller
--- r6bot/r6bot_description/launch/view_r6bot.launch.py
+++ r6bot/r6bot_description/launch/view_r6bot.launch.py
@@ -1,6 +1,5 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
