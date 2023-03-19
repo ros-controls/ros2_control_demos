bfac8e8fb93e93d18fe6d679b1d7c222665e655d
Christoph Froehlich <christoph.froehlich@spaceteam.at>
2023-03-19 21:51:56 +0000

    Fix merge conflicts

--- ros2_control_demo_bringup/launch/rrbot.launch.py
+++ ros2_control_demo_bringup/launch/rrbot.launch.py
@@ -87,13 +87,8 @@ def generate_launch_description():
 
     robot_controller_spawner = Node(
         package="controller_manager",
-<<<<<<< HEAD
-        executable="spawner",
-        arguments=["forward_position_controller", "-c", "/controller_manager"],
-=======
         executable="spawner.py",
         arguments=["forward_position_controller", "--controller-manager", "/controller_manager"],
->>>>>>> f43fa8a (Rename position_trajectory_controller to joint_trajectory_position_controller (#190))
     )
 
     # Delay rviz start after `joint_state_broadcaster`
