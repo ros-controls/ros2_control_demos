a274eb8fefa0fba88ebda7cc05babb3661dead80
Christoph Froehlich <christoph.froehlich@ait.ac.at>
2023-08-13 09:25:54 +0000

    Extract rrbot from example 1+3

--- example_3/description/launch/view_robot.launch.py
+++ example_3/description/launch/view_robot.launch.py
@@ -27,7 +27,7 @@ def generate_launch_description():
     declared_arguments.append(
         DeclareLaunchArgument(
             "description_package",
-            default_value="ros2_control_demo_example_3",
+            default_value="ros2_control_demo_description",
             description="Description package with robot URDF/xacro files. Usually the argument \
         is not set, it enables use of a custom description.",
         )
@@ -69,7 +69,7 @@ def generate_launch_description():
             PathJoinSubstitution([FindExecutable(name="xacro")]),
             " ",
             PathJoinSubstitution(
-                [FindPackageShare(description_package), "urdf", description_file]
+                [FindPackageShare("ros2_control_demo_example_3"), "urdf", description_file]
             ),
             " ",
             "prefix:=",
