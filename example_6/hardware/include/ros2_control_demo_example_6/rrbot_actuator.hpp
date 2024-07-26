f92cc213316d439f90da6a74b32ec667b7dc8043
Christoph Fröhlich <christophfroehlich@users.noreply.github.com>
2024-07-26 14:10:26 +0200

    Remove visibility macros (#539)
    * Remove visibility macros
    
    * Remove dead target_compile_definitions

--- example_6/hardware/include/ros2_control_demo_example_6/rrbot_actuator.hpp
+++ example_6/hardware/include/ros2_control_demo_example_6/rrbot_actuator.hpp
@@ -29,7 +29,6 @@
 #include "hardware_interface/system_interface.hpp"
 #include "hardware_interface/types/hardware_interface_return_values.hpp"
 #include "rclcpp/macros.hpp"
-#include "ros2_control_demo_example_6/visibility_control.h"
 
 namespace ros2_control_demo_example_6
 {
@@ -38,29 +37,22 @@ class RRBotModularJoint : public hardware_interface::ActuatorInterface
 public:
   RCLCPP_SHARED_PTR_DEFINITIONS(RRBotModularJoint);
 
-  ROS2_CONTROL_DEMO_EXAMPLE_6_PUBLIC
   hardware_interface::CallbackReturn on_init(
     const hardware_interface::HardwareInfo & info) override;
 
-  ROS2_CONTROL_DEMO_EXAMPLE_6_PUBLIC
   std::vector<hardware_interface::StateInterface> export_state_interfaces() override;
 
-  ROS2_CONTROL_DEMO_EXAMPLE_6_PUBLIC
   std::vector<hardware_interface::CommandInterface> export_command_interfaces() override;
 
-  ROS2_CONTROL_DEMO_EXAMPLE_6_PUBLIC
   hardware_interface::CallbackReturn on_activate(
     const rclcpp_lifecycle::State & previous_state) override;
 
-  ROS2_CONTROL_DEMO_EXAMPLE_6_PUBLIC
   hardware_interface::CallbackReturn on_deactivate(
     const rclcpp_lifecycle::State & previous_state) override;
 
-  ROS2_CONTROL_DEMO_EXAMPLE_6_PUBLIC
   hardware_interface::return_type read(
     const rclcpp::Time & time, const rclcpp::Duration & period) override;
 
-  ROS2_CONTROL_DEMO_EXAMPLE_6_PUBLIC
   hardware_interface::return_type write(
     const rclcpp::Time & time, const rclcpp::Duration & period) override;
 
