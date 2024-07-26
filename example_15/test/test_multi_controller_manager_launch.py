3323be325c558ecc5ded7bc7fab36c36a13083b4
Christoph Froehlich <christoph.froehlich@ait.ac.at>
2024-07-26 21:21:56 +0000

    Revert "Activate assertExitCodes tests"
    This reverts commit fe28c67cb2625fbff37bd4417af8b131e5f5ee4c.

--- example_15/test/test_multi_controller_manager_launch.py
+++ example_15/test/test_multi_controller_manager_launch.py
@@ -39,7 +39,7 @@ from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_testing.actions import ReadyToTest
 
-import launch_testing.markers
+# import launch_testing.markers
 import rclpy
 from rclpy.node import Node
 
@@ -96,10 +96,11 @@ class TestFixture(unittest.TestCase):
         check_if_js_published("/rrbot_2/joint_states", ["rrbot_2_joint1", "rrbot_2_joint2"])
 
 
-@launch_testing.post_shutdown_test()
-# These tests are run after the processes in generate_test_description() have shutdown.
-class TestDescriptionCraneShutdown(unittest.TestCase):
+# TODO(anyone): enable this if shutdown of ros2_control_node does not fail anymore
+# @launch_testing.post_shutdown_test()
+# # These tests are run after the processes in generate_test_description() have shutdown.
+# class TestDescriptionCraneShutdown(unittest.TestCase):
 
-    def test_exit_codes(self, proc_info):
-        """Check if the processes exited normally."""
-        launch_testing.asserts.assertExitCodes(proc_info)
+#     def test_exit_codes(self, proc_info):
+#         """Check if the processes exited normally."""
+#         launch_testing.asserts.assertExitCodes(proc_info)
