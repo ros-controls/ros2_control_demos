ea65f918edb42954b3eb2ff5ae8d51f57c4d7bb4
Christoph Fröhlich <christophfroehlich@users.noreply.github.com>
2024-07-30 09:14:17 +0200

    Add launch tests to all examples (#540)
    * Add test for ex1
    
    * Add test for ex2
    
    * Add test for ex3
    
    * Add test for ex4
    
    * Add missing dependency
    
    * Deactivate GUI
    
    * Add test for ex5
    
    * Add act stuff to gitignore
    
    * Deactivate gui in tests
    
    * Reduce update rate to avoid clogging the log
    
    * Use set instead of list
    
    * Add test for ex6
    
    * Fix xacro macros and add gui launch argument
    
    * Add test for example_7
    
    * Add test for example_8
    
    * Add test for example_9
    
    * Add test for example_10
    
    * Add test for example_11
    
    * Add test for example_12
    
    * Add test for example_14
    
    * Update test for example_15
    
    * Add test for example_15 multi_cm
    
    * Add missing dependency
    
    * Add missing dependency for ex4+ex5
    
    * Robustify tests and reuse methos for launch_testing
    
    * Fix dependencies
    
    * Use a set to compare the joint names
    
    * Reorder controller spawners
    
    * Activate assertExitCodes tests
    
    * Deactivate failing tests for example_15
    
    * Increase timeout for example_10
    
    * Add backward_ros everywhere
    
    * Add error output and update includes
    
    * Revert "Activate assertExitCodes tests"
    
    This reverts commit fe28c67cb2625fbff37bd4417af8b131e5f5ee4c.
    
    * Add missing import
    
    * Update ros2_control_demo_testing/package.xml
    
    Co-authored-by: Sai Kishor Kothakota <sai.kishor@pal-robotics.com>
    
    * Reuse check_node_running
    
    * Try to robustify example_14 tests
    
    ---------
    
    Co-authored-by: Sai Kishor Kothakota <sai.kishor@pal-robotics.com>
    (cherry picked from commit 0ba16d24595ad1a221a2726f697783a961019c7c)

---
+++ ros2_control_demo_testing/setup.py
@@ -0,0 +1,53 @@
+# Copyright (c) 2024 AIT - Austrian Institute of Technology GmbH
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#
+#    * Neither the name of the {copyright_holder} nor the names of its
+#      contributors may be used to endorse or promote products derived from
+#      this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# Author: Christoph Froehlich
+
+from setuptools import find_packages, setup
+
+package_name = "ros2_control_demo_testing"
+
+setup(
+    name=package_name,
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="Christoph Froehlich",
+    maintainer_email="christoph.froehlich@ait.ac.at",
+    description="Utilities for launch testing",
+    license="Apache-2.0",
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": [],
+    },
+)
