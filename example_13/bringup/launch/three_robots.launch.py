5ecaac43a5e03d97c6ab8a4b055a34a381a64eba
Christoph Froehlich <christoph.froehlich@ait.ac.at>
2024-12-16 15:53:35 +0000

    Use robot_description from topic

--- example_13/bringup/launch/three_robots.launch.py
+++ example_13/bringup/launch/three_robots.launch.py
@@ -88,7 +88,10 @@ def generate_launch_description():
     control_node = Node(
         package="controller_manager",
         executable="ros2_control_node",
-        parameters=[robot_description, robot_controllers],
+        parameters=[robot_controllers],
+        remappings=[
+            ("~/robot_description", "/robot_description"),
+        ],
     )
     robot_state_pub_node = Node(
         package="robot_state_publisher",
