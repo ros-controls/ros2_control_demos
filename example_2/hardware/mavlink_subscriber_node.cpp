3614d5f116900cd3af3075ced1f7207ecf989986
Samuel <stri0019@student.monash.edu>
2025-05-06 16:08:52 +1000

    Added a mavlink_message_t declaration

--- example_2/hardware/mavlink_subscriber_node.cpp
+++ example_2/hardware/mavlink_subscriber_node.cpp
@@ -8,6 +8,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 
+// #include "mavros_msgs/msg/mavlink.hpp"
 extern "C" {
   #include <mavlink/v2.0/common/mavlink.h>
 }
@@ -29,6 +30,25 @@ private:
   void mavlink_vel_callback(const geometry_msgs::msg::Twist::SharedPtr msg) {
     RCLCPP_INFO(this->get_logger(), "Received cmd_vel: linear.x = %f, angular.z = %f",
                 msg->linear.x, msg->angular.z);
+
+    mavlink_message_t mavlink_msg;
+
+    mavlink_set_position_target_local_ned_t target;
+
+    // Fill the target message with received velocities
+    target.time_boot_ms = 0; // Optional: time field (use 0 for now)
+    target.target_system = 1; // System ID
+    target.target_component = 1; // Component ID
+    target.coordinate_frame = MAV_FRAME_LOCAL_NED;
+    // target.type_mask = MAVLINK_MSG_SET_POSITION_TARGET_LOCAL_NED_T_MASK_VELOCITY;
+
+    target.vx = msg->linear.x;  // Linear X velocity (m/s)
+    target.vy = 0;              // Linear Y velocity (m/s)
+    target.vz = 0;              // Linear Z velocity (m/s)
+    target.afx = 0;             // Accelerations
+    target.afy = 0;
+    target.afz = 0;
+    target.yaw = msg->angular.z;  // Angular velocity (yaw)
   }
 
   rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr mavlink_vel_subscriber_;
