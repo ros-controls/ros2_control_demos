c290190b2ce9aed981cfb960d1fdb513ab1818ad
Soham Patil <sohampatil45939@gmail.com>
2025-07-20 13:43:05 +0000

    slight reordering

--- example_17/hardware/rrbot.cpp
+++ example_17/hardware/rrbot.cpp
@@ -41,6 +41,9 @@ hardware_interface::CallbackReturn RRBotSystemPositionOnlyHardware::on_init(
 
   // BEGIN: This part here is for exemplary purposes - Please do not copy to your production code
   hw_start_sec_ = stod(info_.hardware_parameters["example_param_hw_start_duration_sec"]);
+  hw_stop_sec_ = stod(info_.hardware_parameters["example_param_hw_stop_duration_sec"]);
+  hw_slowdown_ = stod(info_.hardware_parameters["example_param_hw_slowdown"]);
+  RCLCPP_INFO(get_logger(), "Robot hardware_component update_rate is %dHz", info_.rw_rate);
 
   // Get Weak Pointer to Executor from HardwareComponentInterfaceParams
   executor_ = params.executor;
@@ -58,9 +61,6 @@ hardware_interface::CallbackReturn RRBotSystemPositionOnlyHardware::on_init(
 
     locked_executor->add_node(custom_status_node_->get_node_base_interface());
   }
-  hw_stop_sec_ = stod(info_.hardware_parameters["example_param_hw_stop_duration_sec"]);
-  hw_slowdown_ = stod(info_.hardware_parameters["example_param_hw_slowdown"]);
-  RCLCPP_INFO(get_logger(), "Robot hardware_component update_rate is %dHz", info_.rw_rate);
   // END: This part here is for exemplary purposes - Please do not copy to your production code
 
   for (const hardware_interface::ComponentInfo & joint : info_.joints)
