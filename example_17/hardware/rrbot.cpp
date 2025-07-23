c207444259612e98ab25f66c605365af2bedbbaa
Soham Patil <sohampatil45939@gmail.com>
2025-07-23 14:42:50 +0000

    fix build errors

--- example_17/hardware/rrbot.cpp
+++ example_17/hardware/rrbot.cpp
@@ -41,10 +41,10 @@ hardware_interface::CallbackReturn RRBotSystemPositionOnlyHardware::on_init(
 
   // BEGIN: This part here is for exemplary purposes - Please do not copy to your production code
   hw_start_sec_ =
-    stod(get_hardware_info().hardware_parameters["example_param_hw_start_duration_sec"]);
+    stod(get_hardware_info().hardware_parameters.at("example_param_hw_start_duration_sec"));
   hw_stop_sec_ =
-    stod(get_hardware_info().hardware_parameters["example_param_hw_stop_duration_sec"]);
-  hw_slowdown_ = stod(get_hardware_info().hardware_parameters["example_param_hw_slowdown"]);
+    stod(get_hardware_info().hardware_parameters.at("example_param_hw_stop_duration_sec"));
+  hw_slowdown_ = stod(get_hardware_info().hardware_parameters.at("example_param_hw_slowdown"));
   RCLCPP_INFO(
     get_logger(), "Robot hardware_component update_rate is %dHz", get_hardware_info().rw_rate);
 
