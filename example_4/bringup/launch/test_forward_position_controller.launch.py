fa2fb8351dd67fc7a022710186b4a852cc797616
philipchurchley <philipchurchley@philip-laptop.>
2025-11-24 18:10:53 -0500

    clean example 4

--- example_4/bringup/launch/test_forward_position_controller.launch.py
+++ example_4/bringup/launch/test_forward_position_controller.launch.py
@@ -20,21 +20,21 @@ from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
 
-    position_goals = PathJoinSubstitution(
-        [
-            FindPackageShare("ros2_control_demo_example_4"),
-            "config",
-            "rrbot_forward_position_publisher.yaml",
-        ]
-    )
-
     return LaunchDescription(
         [
             Node(
                 package="ros2_controllers_test_nodes",
                 executable="publisher_forward_position_controller",
                 name="publisher_forward_position_controller",
-                parameters=[position_goals],
+                parameters=[
+                    PathJoinSubstitution(
+                        [
+                            FindPackageShare("ros2_control_demo_example_4"),
+                            "config",
+                            "rrbot_forward_position_publisher.yaml",
+                        ]
+                    )
+                ],
                 output="both",
             )
         ]
