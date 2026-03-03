649b172490d5dd41ca0ae1f836be041ebea0cf03
Christoph Fröhlich <christophfroehlich@users.noreply.github.com>
2026-03-03 08:58:38 +0100

    Disable GUI in tests (#1062)
    (cherry picked from commit 6501c776db18860cac9cd2f13ec0d5a12379bde1)
    
    # Conflicts:
    #	example_16/test/test_view_robot_launch.py
    #	example_17/test/test_view_robot_launch.py

--- example_16/test/test_view_robot_launch.py
+++ example_16/test/test_view_robot_launch.py
@@ -44,7 +44,7 @@ def generate_test_description():
     launch_include = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(
-                get_package_share_directory("ros2_control_demo_example_12"),
+                get_package_share_directory("ros2_control_demo_example_16"),
                 "launch/view_robot.launch.py",
             )
         ),
