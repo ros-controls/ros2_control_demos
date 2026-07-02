7c2d6751c78faad59dfb6c610f0ca968571e3137
adminlip <adminlyh@gmail.com>
2026-07-02 18:51:21 +0800

    Remove the extra xacro path substitution (#1120)

--- example_3/bringup/launch/rrbot_system_multi_interface.launch.py
+++ example_3/bringup/launch/rrbot_system_multi_interface.launch.py
@@ -26,7 +26,7 @@ def generate_launch_description():
     # Get URDF via xacro
     robot_description_content = Command(
         [
-            PathJoinSubstitution([FindExecutable(name="xacro")]),
+            FindExecutable(name="xacro"),
             " ",
             PathJoinSubstitution(
                 [
