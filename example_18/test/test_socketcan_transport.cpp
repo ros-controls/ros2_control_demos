4e5abec2fedf10218421184c7e039c67e27683d8
Quchaosheng <quchaosheng000406@163.com>
2026-09-23 09:25:52 +0800

    Fix the CI checks of the SocketCAN example
    The CMakeLists.txt installed description/launch, which this package does not
    have (its launch files live in bringup/launch). CMake aborts the install step
    with
    
      file INSTALL cannot find ".../description/launch": No such file or directory
    
    which makes the binary job fail as well. The line was copied from example_16
    and example_17, which both have a description/launch directory.
    
    The remaining changes are what pre-commit asks for: wrapped the two overlong
    CMake lines, added the missing copyright headers, fixed the header guards in
    the two new headers, one using-directive in the transport test, and black.

--- example_18/test/test_socketcan_transport.cpp
+++ example_18/test/test_socketcan_transport.cpp
@@ -1,8 +1,31 @@
+// Copyright 2026 ros2_control Development Team
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include <gtest/gtest.h>
+
 #include <cmath>
 #include <limits>
+#include <string>
+
 #include "ros2_control_demo_example_18/socketcan_transport.hpp"
-using namespace ros2_control_demo_example_18;
+
+using ros2_control_demo_example_18::CanTransport;
+using ros2_control_demo_example_18::decode_wheel_feedback;
+using ros2_control_demo_example_18::encode_wheel_command;
+using ros2_control_demo_example_18::SocketCanTransport;
+using ros2_control_demo_example_18::WheelCommand;
+using ros2_control_demo_example_18::WheelFeedback;
 
 class FakeTransport final : public CanTransport
 {
